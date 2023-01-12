4640e33a5dd7fd05f50d00513cf1d65f31ae3166
fakemonkgin <ginli2013@gmail.com>
2023-01-12 16:15:13 +0800

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
             <div className='header__content'>
                 <h1>Hi, I'm Jing Li</h1>
                 <p>Blockchain Developer</p>
-                <button className='button'>Get In Touch</button>
+                <button className='button'>Tel: 18823417610</button>
             </div>
         </section>
     );
