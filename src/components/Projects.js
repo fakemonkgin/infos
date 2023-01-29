aa4b7206ef3f407972ffe1d12315b338aa893a86
fakemonkgin <ginli2013@gmail.com>
2023-01-30 00:33:07 +0800

    update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 // Import Assets
 import nft from '../assets/nft.png';
+import amazon from "../assets/amazon.png";
 
 const Projects = () => {
     return (
@@ -18,6 +19,18 @@ const Projects = () => {
 
                     <a href="https://ebaynftmarketplace.vercel.app/" target="_blank" className="button">Site</a>
                     <a href="https://github.com/fakemonkgin/ebaynftmarketplace" target="_blank" className="button">Code</a>
+                </div>
+
+                <div className="projects__card">
+                    <h3>Web3 Amazon Ecommerce</h3>
+                    <img src={amazon} alt="E-commerce" />
+                    <p>
+                        React/Solidity/hardhat/ethers  <br />
+                        write solidity smart contract with no template, connect wallet, list items, purchase items
+                    </p>
+
+                    <a href="https://web3amazon.vercel.app/" target="_blank" className="button">Site</a>
+                    <a href="https://github.com/fakemonkgin/web3amazon" target="_blank" className="button">Code</a>
                 </div>
             </div>
         </section>
