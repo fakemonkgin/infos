b754b3210f9c1d8d2c914efcc2d42ffd6a2c203c
fakemonkgin <ginli2013@gmail.com>
2025-05-23 10:37:56 +0800

    fix

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -26,32 +26,6 @@ const Projects = () => {
                 </div>
 
                 <div className="projects__card">
-                    <h3>Ebay NFT Marketplace</h3>
-                    <img src={nft} alt="NFT Landing Page" />
-                    <p>
-                        Next.js/Tailwindcss/Thirdweb  <br />
-                        Add to inventory(create items), list items, auction listing & direct listing, make a bid or purchase items
-                    </p>
-                <div className="button-container">
-                    <a href="https://ebaynftmarketplace.vercel.app/" target="_blank" className="button">Site</a>
-                    <a href="https://github.com/fakemonkgin/ebaynftmarketplace" target="_blank" className="button">Code</a>
-                </div>
-                </div>
-
-                <div className="projects__card">
-                    <h3>Web3 Amazon Ecommerce</h3>
-                    <img src={amazon} alt="E-commerce" />
-                    <p>
-                        React/Solidity/hardhat/ethers  <br />
-                        write solidity smart contract with no template, connect wallet, list items, purchase items
-                    </p>
-                <div className="button-container">
-                    <a href="https://web3amazon.vercel.app/" target="_blank" className="button">Site</a>
-                    <a href="https://github.com/fakemonkgin/web3amazon" target="_blank" className="button">Code</a>
-                </div>
-                </div>
-
-                <div className="projects__card">
                     <h3>Web3 Domain Name</h3>
                     <img src={domain} alt="Domain" />
                     <p>
@@ -61,6 +35,19 @@ const Projects = () => {
                 <div className="button-container">
                     <a href="https://web3domainname.vercel.app/" target="_blank" className="button">Site</a>
                     <a href="https://github.com/fakemonkgin/web3domainname" target="_blank" className="button">Code</a>
+                </div>
+                </div>
+
+                <div className="projects__card">
+                    <h3>Ebay NFT Marketplace</h3>
+                    <img src={nft} alt="NFT Landing Page" />
+                    <p>
+                        Next.js/Tailwindcss/Thirdweb  <br />
+                        Add to inventory(create items), list items, auction listing & direct listing, make a bid or purchase items
+                    </p>
+                <div className="button-container">
+                    <a href="https://ebaynftmarketplace.vercel.app/" target="_blank" className="button">Site</a>
+                    <a href="https://github.com/fakemonkgin/ebaynftmarketplace" target="_blank" className="button">Code</a>
                 </div>
                 </div>
             </div>
